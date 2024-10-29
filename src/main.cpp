b2171bfecb8cbb1dfb55c4e92522bea776224b80
wangzhongqing <wangzhongqing0303@qq.com>
2024-10-29 11:14:37 +0800

    chore: init

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+const int dataPin = 1;        // dht11的data引脚
+DHT dhtClass(dataPin, DHT11); // 初始化类
+void setup()
+{
+  Serial.begin(9600);
+  dhtClass.begin(DHT11);
+}
+
+void loop()
+{
+  // 读取温度值
+  float dhtTemperature = dhtClass.readTemperature();
+  // 读取湿度值
+  float dhtHumidity = dhtClass.readHumidity();
+  if (isnan(dhtTemperature) || isnan(dhtHumidity))
+  {                                   // 如果读取错误
+    Serial.println("读取传感器失败"); // 读取失败提示
+    return;
+  }
+
+  delay(8000);
+  Serial.print("我是温度");
+  Serial.print(dhtTemperature);
+  Serial.print("我是湿度");
+  Serial.print(dhtHumidity);
+  Serial.print("\n");
+}
